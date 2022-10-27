8f61ddfcf708a2cb5ffdd16ee508d0e1a0751019
TomaszOnopa <tomaszonopa.pl@gmail.com>
2022-10-27 12:39:35 +0200

    dodanie obsługi parametru cmd

--- lab2/source/index.php
+++ lab2/source/index.php
@@ -1,1 +1,10 @@
-<?php echo "Hello World!\n"; ?>
+<?php 
+	if ($_GET["cmd"] == "time") {
+		echo "TODO: time service";
+	}
+	elseif ($_GET["cmd"] == "rev") {
+		echo "TODO: rev service";
+	}
+	else
+		echo "Hello World!\n"; 
+?>
