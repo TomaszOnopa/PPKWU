15ba60e3a5f7e2f65ffd2f8d71375e7568780505
TomaszOnopa <tomekonopa.pl@gmail.com>
2022-11-24 12:55:17 +0100

    tworzenie pliku json

--- lab4/source/index.php
+++ lab4/source/index.php
@@ -1,7 +1,17 @@
 <?php
 	if (isset($_GET["num1"]) && isset($_GET["num2"])) {
-		if ($_GET["num2"] == 0)
+		$num1 = (int)$_GET["num1"];
+		$num2 = (int)$_GET["num2"];
+		if ($num2 == 0) {
 			echo "num2 nie może być równe zero";
+		}
+		else {
+			echo json_encode(array(
+			"sum" => $num1+$num2,
+			"sub" => $num1-$num2,
+			"nul" => $num1*$num2,
+			"div" => (int)($num1/$num2)));
+		}
 	}
 	else
 		echo "Nie podano parametrów";
