4d6dda93dec5c70c86f3424a5b41fa80fdc3083a
TomaszOnopa <tomekonopa.pl@gmail.com>
2022-11-24 12:40:06 +0100

    sprawdzanie czy podano oba parametry

--- lab4/source/index.php
+++ lab4/source/index.php
@@ -1,1 +1,8 @@
-<?php echo "Hello World!\n"; ?>
+<?php
+	if (isset($_GET["num1"]) && isset($_GET["num2"])) {
+		if ($_GET["num2"] == 0)
+			echo "num2 nie może być równe zero";
+	}
+	else
+		echo "Nie podano parametrów";
+?>
