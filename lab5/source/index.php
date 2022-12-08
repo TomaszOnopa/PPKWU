96b5dcb92f37821f0bef56199ffe0cbddf564201
student <tomekonopa.pl@gmail.com>
2022-12-08 13:18:27 +0100

    wykonywanie obliczen na num1 i num2

--- lab5/source/index.php
+++ lab5/source/index.php
@@ -1,7 +1,7 @@
 <?php
 	$json_data = file_get_contents('php://input');
 	$data = json_decode($json_data);
-	if (property_exists($data, "str")) {
+	if (property_exists($data, "str") && !property_exists($data, "num1") && !property_exists($data, "num2") ) {
 		$str = $data->{'str'};
 		
 		$lowercase = 0;
@@ -10,7 +10,7 @@
 		$special = 0;
 		
 		$str_array = str_split($str, 1);
-		foreach ($str_array as $value) {
+		/*foreach ($str_array as $value) {
 			if (ctype_upper($value)) {
 				$uppercase += 1;
 			}
@@ -28,14 +28,25 @@
 			"lowercase" => $lowercase,
 			"uppercase" => $uppercase,
 			"digit" => $digit,
-			"special" => $special));
+			"special" => $special));*/
 	}
-	if (property_exists($data, "num1") && property_exists($data, "num2")) {
+	else if (!property_exists($data, "str") && property_exists($data, "num1") && property_exists($data, "num2")) {
 		$num1 = $data->{'num1'};
 		$num2 = $data->{'num2'};
-		echo "num1,num2";
+		
+		if ($num2 == 0) {
+			echo "num2 nie może być równe zero";
+		}
+		else {
+			echo json_encode(array(
+			"sum" => $num1+$num2,
+			"sub" => $num1-$num2,
+			"mul" => $num1*$num2,
+			"div" => (int)($num1/$num2),
+			"mod" => $num1%$num2));
+		}
 	}
-	if (property_exists($data, "str") && property_exists($data, "num1") && property_exists($data, "num2")) {
+	else if (property_exists($data, "str") && property_exists($data, "num1") && property_exists($data, "num2")) {
 		
 	}
 ?>
