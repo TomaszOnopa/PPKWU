9b66ba064799da92f5836af83afab2187a9d5ae6
student <tomekonopa.pl@gmail.com>
2022-12-08 12:56:33 +0100

    sprawdzanie zawartosci przeslanego pliku

--- lab5/source/index.php
+++ lab5/source/index.php
@@ -1,5 +1,10 @@
 <?php
 	$json_data = file_get_contents('php://input');
 	$data = json_decode($json_data);
-	echo $data->{'str'};
+	if (property_exists($data, "str")) {
+		echo "str";
+	}
+	if (property_exists($data, "num1") && property_exists($data, "num2")) {
+		echo "num1,num2";
+	}
 ?>
