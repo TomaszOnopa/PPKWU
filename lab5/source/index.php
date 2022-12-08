960eec2a7343bb5465a54648853264e97a48bd69
student <tomekonopa.pl@gmail.com>
2022-12-08 13:39:27 +0100

    obsluga json'a, ktory ma jednoczesnie parametry str, num1 i num2

--- lab5/source/index.php
+++ lab5/source/index.php
@@ -10,25 +10,27 @@
 		$special = 0;
 		
 		$str_array = str_split($str, 1);
-		/*foreach ($str_array as $value) {
-			if (ctype_upper($value)) {
+		$len = count($str_array);
+		for ($i = 0; $i < $len; $i++) {
+			if (ctype_upper($str_array[$i])) {
 				$uppercase += 1;
+				continue;
 			}
-			else if (ctype_lower($value)) {
+			if (ctype_lower($str_array[$i])) {
 				$lowercase += 1;
+				continue;
 			}
-			else if (is_numeric($value)) {
+			/*if (is_int($str_array[$i])) {
 				$digit += 1;
+				continue;
 			}
-    		else {
-    			$special += 1;
-    		}
+	    	$special += 1;*/
 		}
 		echo json_encode(array(
 			"lowercase" => $lowercase,
 			"uppercase" => $uppercase,
 			"digit" => $digit,
-			"special" => $special));*/
+			"special" => $special));
 	}
 	else if (!property_exists($data, "str") && property_exists($data, "num1") && property_exists($data, "num2")) {
 		$num1 = $data->{'num1'};
@@ -47,6 +49,48 @@
 		}
 	}
 	else if (property_exists($data, "str") && property_exists($data, "num1") && property_exists($data, "num2")) {
+		$str = $data->{'str'};
 		
+		$lowercase = 0;
+		$uppercase = 0;
+		$digit = 0;
+		$special = 0;
+		
+		$str_array = str_split($str, 1);
+		$len = count($str_array);
+		for ($i = 0; $i < $len; $i++) {
+			if (ctype_upper($str_array[$i])) {
+				$uppercase += 1;
+				continue;
+			}
+			if (ctype_lower($str_array[$i])) {
+				$lowercase += 1;
+				continue;
+			}
+			/*if (is_int($str_array[$i])) {
+				$digit += 1;
+				continue;
+			}
+	    	$special += 1;*/
+		}
+		
+		$num1 = $data->{'num1'};
+		$num2 = $data->{'num2'};
+		
+		if ($num2 == 0) {
+			echo "num2 nie może być równe zero";
+		}
+		else {
+			echo json_encode(array(
+			"lowercase" => $lowercase,
+			"uppercase" => $uppercase,
+			"digit" => $digit,
+			"special" => $special,
+			"sum" => $num1+$num2,
+			"sub" => $num1-$num2,
+			"mul" => $num1*$num2,
+			"div" => (int)($num1/$num2),
+			"mod" => $num1%$num2));
+		}
 	}
 ?>
