ff009558f0c321b43a6e0e7d7d993780e05bd35a
TomaszOnopa <tomekonopa.pl@gmail.com>
2022-12-22 12:44:03 +0100

    zliczanie znakow w str

--- lab6/source/index.php
+++ lab6/source/index.php
@@ -4,11 +4,37 @@
 	$num1 = $xml->num1;
 	$num2 = $xml->num2;
 	$str = $xml->str;
-	$array = array(
-			"sum" => $num1+$num2,
-			"sub" => $num1-$num2,
-			"mul" => $num1*$num2,
-			"div" => (int)($num1/$num2),
-			"mod" => $num1%$num2);
 	
+	$lowercase = 0;
+	$uppercase = 0;
+	$digit = 0;
+	$special = 0;
+	
+	$str_array = str_split($str, 1);
+	$len = count($str_array);
+	for ($i = 0; $i < $len; $i++) {
+		if (ctype_upper($str_array[$i])) {
+			$uppercase += 1;
+			continue;
+		}
+		if (ctype_lower($str_array[$i])) {
+			$lowercase += 1;
+			continue;
+		}
+		/*if (is_int($str_array[$i])) {
+			$digit += 1;
+			continue;
+		}
+    	$special += 1;*/
+	}
+	echo json_encode(array(
+		"lowercase" => $lowercase,
+		"uppercase" => $uppercase,
+		"digit" => $digit,
+		"special" => $special,
+		"sum" => $num1+$num2,
+		"sub" => $num1-$num2,
+		"mul" => $num1*$num2,
+		"div" => (int)($num1/$num2),
+		"mod" => $num1%$num2));
 ?>
