f4609da1d07b85e55b04958a5c148a4869ba7af3
TomaszOnopa <tomekonopa.pl@gmail.com>
2022-12-22 13:12:28 +0100

    wyswietlenie wyniku w formacie xml

--- lab6/source/index.php
+++ lab6/source/index.php
@@ -1,4 +1,48 @@
 <?php
+	function xml_encode($mixed, $domElement=null, $DOMDocument=null) {
+		if (is_null($DOMDocument)) {
+		    $DOMDocument =new DOMDocument;
+		    $DOMDocument->formatOutput = true;
+		    xml_encode($mixed, $DOMDocument, $DOMDocument);
+		    echo $DOMDocument->saveXML();
+		}
+		else {
+		    // To cope with embedded objects 
+		    if (is_object($mixed)) {
+		      $mixed = get_object_vars($mixed);
+		    }
+		    if (is_array($mixed)) {
+		        foreach ($mixed as $index => $mixedElement) {
+		            if (is_int($index)) {
+		                if ($index === 0) {
+		                    $node = $domElement;
+		                }
+		                else {
+		                    $node = $DOMDocument->createElement($domElement->tagName);
+		                    $domElement->parentNode->appendChild($node);
+		                }
+		            }
+		            else {
+		                $plural = $DOMDocument->createElement($index);
+		                $domElement->appendChild($plural);
+		                $node = $plural;
+		                if (!(rtrim($index, 's') === $index)) {
+		                    $singular = $DOMDocument->createElement(rtrim($index, 's'));
+		                    $plural->appendChild($singular);
+		                    $node = $singular;
+		                }
+		            }
+
+		            xml_encode($mixedElement, $node, $DOMDocument);
+		        }
+		    }
+		    else {
+		        $mixed = is_bool($mixed) ? ($mixed ? 'true' : 'false') : $mixed;
+		        $domElement->appendChild($DOMDocument->createTextNode($mixed));
+		    }
+		}
+	}
+
 	$xmlString = file_get_contents('php://input');
 	$xml = new SimpleXMLElement($xmlString);
 	
@@ -30,7 +74,7 @@
 			}
 	    	$special += 1;*/
 		}
-		echo json_encode(array(
+		echo xml_encode(array(
 			"lowercase" => $lowercase,
 			"uppercase" => $uppercase,
 			"digit" => $digit,
@@ -64,7 +108,7 @@
 			}
 	    	$special += 1;*/
 		}
-		echo json_encode(array(
+		echo xml_encode(array(
 			"lowercase" => $lowercase,
 			"uppercase" => $uppercase,
 			"digit" => $digit,
@@ -72,7 +116,7 @@
 	}
 	
 	if (isset($xml->num1) && isset($xml->num1) && !isset($xml->str)) {
-		echo json_encode(array(
+		echo xml_encode(array(
 			"sum" => $num1+$num2,
 			"sub" => $num1-$num2,
 			"mul" => $num1*$num2,
