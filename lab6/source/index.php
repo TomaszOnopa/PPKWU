5220f7aec388accdefbbf9c1242581c7d97214f6
TomaszOnopa <tomekonopa.pl@gmail.com>
2022-12-22 12:37:23 +0100

    wykonanie obliczen na num1 i num2

--- lab6/source/index.php
+++ lab6/source/index.php
@@ -1,6 +1,14 @@
 <?php
 	$xmlString = file_get_contents('php://input');
-	//$xml = new SimpleXMLElement($xmlString);
-	//echo $xml->str;
-	echo $xmlString;
+	$xml = new SimpleXMLElement($xmlString);
+	$num1 = $xml->num1;
+	$num2 = $xml->num2;
+	$str = $xml->str;
+	$array = array(
+			"sum" => $num1+$num2,
+			"sub" => $num1-$num2,
+			"mul" => $num1*$num2,
+			"div" => (int)($num1/$num2),
+			"mod" => $num1%$num2);
+	
 ?>
